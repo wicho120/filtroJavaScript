2fcb822607e99a9e7c47a3400f767b908be34606
Wicho120 <35779309+wicho120@users.noreply.github.com>
2024-05-23 14:42:42 -0500

    feat: :tada: Inicio de filtro

---
+++ src/my-element.js
@@ -0,0 +1,38 @@
+import { LitElement, css, html } from 'lit'
+import litLogo from './assets/lit.svg'
+import viteLogo from '/vite.svg'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class MyElement extends LitElement {
+  
+  static get properties() {
+    return {
+      /**
+       * Copy for the read the docs hint.
+       */
+      docsHint: { type: String },
+
+      /**
+       * The number of times the button has been clicked.
+       */
+      count: { type: Number },
+    }
+  }
+
+  constructor() {
+    super()
+    this.docsHint = 'Click on the Vite and Lit logos to learn more'
+    this.count = 0
+  }
+
+  render() {
+  }
+
+}
+
+window.customElements.define('my-element', MyElement)
